33ecf9291ce2b6437f863b840e116dfab849ebff
PURIT <87290907+PURIT777@users.noreply.github.com>
2021-07-19 10:04:05 +0530

    Update Profit and loss.py
    Hi This is profit and loss finding code

--- Profit and loss.py
+++ Profit and loss.py
@@ -3,9 +3,9 @@ print("price and Sell prise.....")
 a=int(input("S.P.= "))
 b=int(input("C.P.= "))
 if (b<a):
-    print("Congo! you have a profit of ", b-a)
+    print("Congo! you have a profit of ", a-b)
 elif(a<b):
-    print("It's a Loss of ", a-b)
+    print("It's a Loss of ", b-a)
 else:
     priint("Invalid entry")
 print("Thanku to use our servise")
